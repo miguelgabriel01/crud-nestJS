2831ecc1703eb07afb5e097ddd1c802e58c204d3
miguelgabriel01 <gabrielogabriel10@gmail.com>
2022-06-24 15:14:52 -0300

    finalização da aplicação

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,1 +1,13 @@
-export class CreateUserDto {}
+import { IsEmail, IsString, MinLength } from 'class-validator'; //importamos atributos de validação de campos
+
+export class CreateUserDto {
+  @IsEmail()
+  email: string;
+
+  @IsString()
+  name: string;
+
+  @IsString()
+  @MinLength(6)
+  password: string;
+}
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,26 +1,64 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { User } from './entities/user.entity';
 
 @Injectable()
 export class UsersService {
+  private users: User[] = [];
+
   create(createUserDto: CreateUserDto) {
-    return 'This action adds a new user';
+    //verificamos se o id já existe, se não, atribuimos 0 a ele.
+    const currentMaxId = this.users[this.users.length - 1]?.id || 0;
+
+    //atribuimos ao campo id a logica
+    const id = currentMaxId + 1;
+
+    //criamos o obj
+    const user = {
+      id,
+      ...createUserDto,
+    };
+
+    this.users.push(user);
+    return user;
   }
 
+  //retorna todod os dados
   findAll() {
-    return `This action returns all users`;
+    return this.users;
   }
 
+  //retornamos apenas um usuario
   findOne(id: number) {
-    return `This action returns a #${id} user`;
+    const index = this.users.findIndex((user) => user.id === id);
+
+    return this.users[index];
   }
 
   update(id: number, updateUserDto: UpdateUserDto) {
-    return `This action updates a #${id} user`;
+    const user = this.findOne(id);
+
+    const newUser = {
+      ...user,
+      ...updateUserDto,
+    };
+
+    const index = this.users.findIndex((user) => user.id === id);
+
+    this.users[index] = newUser;
+
+    return newUser;
   }
 
   remove(id: number) {
-    return `This action removes a #${id} user`;
+    const index = this.users.findIndex((user) => user.id === id);
+
+    if (index === -1) {
+      throw new NotFoundException(`User with id ${id} not  found.`);
+    }
+    this.users.splice(index, 1);
+
+    return;
   }
 }
